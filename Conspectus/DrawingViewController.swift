f95123445aec706c584248203b06901d662da1f7
Fedor <fridrixnm@gmail.com>
2016-07-10 02:43:25 +0300

    Created canvasView

--- Conspectus/DrawingViewController.swift
+++ Conspectus/DrawingViewController.swift
@@ -10,13 +10,29 @@ import UIKit
 
 class DrawingViewController: UIViewController {
 
-    @IBOutlet weak var canvasView: CanvasView!
+	
+    @IBOutlet weak var permanentView: CanvasView!
+	
+	var lastPoint = CGPoint.zero
+	var red: CGFloat = 0.0
+	var green: CGFloat = 0.0
+	var blue: CGFloat = 0.0
+	var brushWidth: CGFloat = 10.0
+	var opacity: CGFloat = 1.0
+	var swiped = false
+	
+	let colors: [(CGFloat, CGFloat, CGFloat)] = [(0, 0, 0)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        canvasView.clearCanvas(animated:false)
-		print("canvas loaded")
+		permanentView.clearCanvas(animated: true)
+		
+		print("permanent loaded")
     }
 
+    @IBAction func Clear(sender: AnyObject) {
+        permanentView.image = nil
+    }
+	
 
 }
