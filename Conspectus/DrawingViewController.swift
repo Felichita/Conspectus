6016921ea91c1269a27bde34ad9481b5791381a5
Fedor <fridrixnm@gmail.com>
2016-07-10 12:59:46 +0300

    Added doubleTap to drawing

--- Conspectus/DrawingViewController.swift
+++ Conspectus/DrawingViewController.swift
@@ -17,8 +17,21 @@ class DrawingViewController: UIViewController {
     override func viewDidLoad() {
 		super.viewDidLoad()
 		permanentView.clearCanvas(animated: true)
+		
+		let tap = UITapGestureRecognizer(target: self, action: #selector(doubleTapped))
+		tap.numberOfTapsRequired = 2
+		view.addGestureRecognizer(tap)
     }
-
+	
+	func doubleTapped() {
+		let imageName = "remark.png"
+		let image = UIImage.init(named: imageName)
+		let imageView = CanvasView(image: image)
+		
+		imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 200)
+		view.addSubview(imageView)
+	}
+	
     @IBAction func Clear(sender: AnyObject) {
 		permanentView.count = 0
 		permanentView.image = nil
